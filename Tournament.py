cb9b87a4e682e96a5e753621c7b21d12dc6d76c3
GANESH CHANDRA <77651439+M-G-C-64@users.noreply.github.com>
2021-05-28 13:05:09 +0530

    Update Tournament.py

--- Tournament.py
+++ Tournament.py
@@ -1,14 +1,18 @@
+def summ(lst):
+    sumlst = [0]*(len(lst)+1)
+    for i in range(1,len(lst)+1):
+        sumlst[i] = lst[i-1]+sumlst[i-1]
+    sumlst.pop(0)
+    return sumlst[::-1]
+
 n = int(input())
 lst = list(map(int, input().split()))
-
 lst.sort()
-tsum = lst[0]
-temp = [tsum]
-for i in range(1,len(lst)):
-    tsum += lst[i]
-    temp.append(tsum)
-count = 0
-for i in range(1, len(lst)):
-    count += ((n-i)*lst[-i])-temp[-(1+i)]
-print(count)
 
+sumlst = summ(lst[::-1])
+trev = 0
+
+for i in range(n-1):
+    trev += abs((lst[i])*(n-1-i) - sumlst[i+1])
+
+print(trev)
